3106857e0317fa10200b6efe1a2074e517a7a5f9
tyagitaimannid <tyagita.imanni.d@mail.ugm.ac.id>
2014-11-10 16:06:30 +0700

    add index

--- xml/Login/index.php
+++ xml/Login/index.php
@@ -1,36 +1,21 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_error',1);
+  //memulai session
+  session_start();
+  //cek adanya session
+  if(ISSET($_SESSION['username'])){
+    //jika tidak ada session 
+  } else
+  header("location:login.php");
+?>
+  <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="asset/css/navbar-fixed-top.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="asset/js/bootstrap.js"> </script> 
+<body>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>Fixed Top Navbar Example for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/navbar-fixed-top.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <!-- Fixed navbar -->
+  <!-- Fixed navbar -->
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
@@ -40,38 +25,16 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">E-learning</a>
+          <a class="navbar-brand" href="#">E-Learning</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo "Anda login sebagai  "; echo $_SESSION['username']; ?> <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                
-                <li class="divider"></li>
-                <li class="dropdown-offset7">
-						<a  href="#"  class="dropdown-toggle" data- toggle="dropdown"></i>
-						<?php echo "Anda login sebagai  "; echo $_SESSION['username']; ?><b class="caret"></b></a>
-				</li>
-				<li>
-						<a  href="logout.php"><i class="icon-off"></i>&nbsp;  &nbsp;&nbsp;Logout</a>
-				</li>
-				</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li><a href="../navbar-static-top/">Static top</a></li>
-            <li class="active"><a href="./">Fixed top</a></li>
+            <li><a href="../navbar-static-top/">Anda login sebagai <?php echo $_SESSION['username'];?></a></li>
+            <li class="active"><a href="logout.php">Logout</a></li>
           </ul>
-		  
-		  
         </div><!--/.nav-collapse -->
       </div>
     </div>
@@ -80,22 +43,12 @@
 
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1>Selamat Datang di Elisa Universitas Gadjah Mada</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <h3>Selamat Datang di ELISA Universitas Gadjah Mada</h3>
+        <p>Mari berbagi pengetahuan, berkolaborasi dan menjadi produktif dalam kerangka pembelajaran.</p>
         <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Selengkapnya &raquo;</a>
         </p>
       </div>
 
     </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="assets/bootstrap.min.js"></script>
-   
-  </body>
-</html>
+</body>
